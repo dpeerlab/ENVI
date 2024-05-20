01361cb47c5ff1406eb6dc21b398a10869fe322b
DoronHav <havivd@ll02.hpc.private>
2024-05-20 12:22:13 -0400

    Bump version to 0.3.5 in conf.py and pyproject.toml

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -37,7 +37,7 @@ copyright = "2024, " + 'Doron Haviv'
 author = 'Doron Haviv'
 
 # The full version, including alpha/beta/rc tags
-release = '0.3.2'
+release = '0.3.5'
 
 
 # -- General configuration ---------------------------------------------------
